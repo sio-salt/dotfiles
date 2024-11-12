21e41e7d4db8b996ee59aa12ef07242b66e7f424
sio-salt <saltinthedesertyou@gmail.com>
2024-11-13 01:13:13 +0900

    add bunch of colorcschemes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1032,12 +1032,25 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme('tokyonight-night')
+      vim.cmd.colorscheme('tokyonight-moon')
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi('Comment gui=none')
     end,
   },
+
+  { 'jacoborus/tender.vim' },
+  { 'tomasr/molokai' },
+  { 'gosukiwi/vim-atom-dark' },
+  { 'dbb/vim-gummybears-colorscheme' },
+  { 'ntk148v/vim-horizon' },
+  { 'yasukotelin/shirotelin' },
+  { 'phanviet/vim-monokai-pro' },
+  { 'sainnhe/everforest' },
+  { 'cocopon/iceberg.vim' },
+  { 'AhmedAbdulrahman/aylin.vim' },
+  { 'doums/darcula' },
+  { 'ellisonleao/gruvbox.nvim' },
 
   -- Highlight todo, notes, etc in comments
   {
