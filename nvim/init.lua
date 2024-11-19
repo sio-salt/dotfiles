d356d09d50b18632d5c6732b2493da8cfbcb6156
sio-salt <saltinthedesertyou@gmail.com>
2024-11-20 00:41:02 +0900

    add copilot chat

--- nvim/init.lua
+++ nvim/init.lua
@@ -1055,6 +1055,20 @@ require('lazy').setup({
   },
 
   {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+
+  {
     'akinsho/toggleterm.nvim',
     version = '*',
     opts = {--[[ things you want to change go here]]
