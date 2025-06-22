e3a8e209d28f649afb3f4005eb3cf20f2ad4b9b7
sio-salt <saltinthedesertyou@gmail.com>
2025-06-22 16:04:36 +0900

    remove wsl_mount

--- shell/.wsl_mount.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if ! mountpoint -q ~/05.Yamadai; then
-    sshfs -o transform_symlinks Yamadai:/home/katoy /home/kato/05.Yamadai
-fi
-if ! mountpoint -q ~/04.Kyusandai; then
-    sshfs -o transform_symlinks Kyusandai:/home/kato /home/kato/04.Kyusandai
-fi
-
